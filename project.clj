c54ca632b0c8acbe6cc90cb6d872cd5c40f29f88
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2015-03-25 21:20:39 +0900

    Initial commit~ :-)

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject examples-lazy-seq "0.1.0-SNAPSHOT"
+  :description "Examples for lazy sequence."
+  :url "https://github.com/vmfhrmfoaj/examples-lazy-seq"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
